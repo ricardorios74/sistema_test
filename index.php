d12f5ab3a926e65f3a467e3a72dcc622227342d9
rrios Local <ricardorios74@gmail.com>
2024-06-07 13:48:48 -0400

    Se agrega index.php y contacto.html

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git</title>
+</head>
+<body>
+    <h1>Proyecto Git</h1> 
+    <h2>Temario del curso de Git</h2>
+    <h1>Example heading<span class="badge badge-primary">New</span></h1>
+    <ul>
+        <li>Introduccion</li>
+        <li>Inicializar repositorio</li>
+        <li>Seguimiento de archivos</li>
+        <li>Hstorial de cambios</li>
+        <li>Deshacer cambios y etiquetas</li>
+    </ul>
+    
+</body>
+</html>
